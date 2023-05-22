03952b059a76a1b505b71ad463b2ce6370253483
JackShepee <salamakhaviktor@gmail.com>
2023-05-22 22:38:48 +0300

    optimization

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const name = prompt("Please enter your name:");
 
-if (name === null || name === "" || !isNaN(name)) {
+if (!name || !isNaN(name)) {
   alert("Please, enter a valid name!");
 } else {
   alert(`Hello, ${name}! How are you?`);
